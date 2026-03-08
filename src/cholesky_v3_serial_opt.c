23f745af4f60d99bbec9392bba912ea2c8687918
Barbara Koch <barbara.koch.bk@gmail.com>
2026-03-08 22:26:05 +0000

    v3

---
+++ src/cholesky_v3_serial_opt.c
@@ -0,0 +1,48 @@
+#include "mphil_dis_cholesky.h"
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+
+#define MAX_N 100000
+
+double mphil_dis_cholesky(double *c, int n)
+{
+    if (n < 1 || n > MAX_N) {
+        fprintf(stderr, "mphil_dis_cholesky: n=%d out of range [1, %d]\n", n, MAX_N);
+        return -1.0;
+    }
+
+    struct timespec t_start, t_end;
+    clock_gettime(CLOCK_MONOTONIC, &t_start);
+
+    for (int p = 0; p < n; p++) {  /* move along the diagonal of the matrix */
+
+        double diag = sqrt(c[p*n + p]);
+        c[p*n + p] = diag;  /* update diagonal element */
+
+        /* replace division by multiplication with reciprocal */
+        double inv_diag = 1.0 / diag;
+
+        /* update row to right of diagonal element */
+        for (int j = p + 1; j < n; j++) {
+            c[p*n + j] *= inv_diag;
+        }
+
+        /* update column below diagonal element */
+        for (int i = p + 1; i < n; i++) {
+            c[i*n + p] *= inv_diag;
+        }
+
+        /* update submatrix below-right of diagonal element */
+        for (int i = p + 1; i < n; i++) {
+            double c_ip = c[i*n + p];
+            for (int j = p + 1; j < n; j++) {
+                c[i*n + j] -= c_ip * c[p*n + j];
+            }
+        }
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &t_end);
+    return (t_end.tv_sec  - t_start.tv_sec)
+         + (t_end.tv_nsec - t_start.tv_nsec) * 1.0e-9;
+}
